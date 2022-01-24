a1947bfae5aa4ed3b61dc3022713bfd5a903e900
Divya2948 <96617321+Divya2948@users.noreply.github.com>
2022-01-24 16:23:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var s1
+function setup() {
+  createCanvas(400,400);
+s1=new Student("Divya",14,8) 
+s1.display()
+s2=new Student("vedika",14,8)
+s2.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
